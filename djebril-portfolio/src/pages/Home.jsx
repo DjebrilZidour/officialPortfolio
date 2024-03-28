86ef06366281792bc2597c4d5adf7b5eed5ca654
Djebril <djebrilacademy@gmail.com>
2024-03-28 01:53:10 +0100

    tru backrounf image

--- djebril-portfolio/src/pages/Home.jsx
+++ djebril-portfolio/src/pages/Home.jsx
@@ -1,18 +1,29 @@
-const Home = ()=>{
-    return(
-        <section className="flex justify-between items-center mx-12 my-8">
-            <div className="flex flex-col items-start justify-center w-1/2 gap-4">
-                <h1 className="text-5xl">Djebril Zidour </h1>
-                <p className="text-3xl"> hi, im djebril im a web developer and this is my portfolio</p>
-                <div className="flex justify-center items-center gap-4">
-                    <button className="border px-4 py-2 ">click me </button>
-                    <button className="border px-4 py-2 "> learn more</button>
-                </div>
-            </div>
-            <div className="w-1/2">
-                <img className="rounded-3xl w-full" src="https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=800" alt="" />
-            </div>
-        </section>
-    )
-}
-export default Home
+const Home = () => {
+  return (
+    <section className="flex justify-between items-center mx-12 my-8 bg-[url(https://i.ibb.co/RPYXJz6/Screen-Shot-2022-12-13-at-5-50-43-PM-copy.png)] bg-no-repeat bg-contain bg-center">
+      <div className="flex flex-col items-start justify-center w-1/2 gap-4">
+        <h1 className="text-5xl">Djebril Zidour </h1>
+        <p className="text-3xl">
+          {" "}
+          hi, im djebril im a web developer and this is my portfolio
+        </p>
+        <div className="flex justify-center items-center gap-4">
+          <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
+            click me{" "}
+          </button>
+          <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
+            {" "}
+            learn more
+          </button>
+        </div>
+      </div>
+      <div className="w-1/2 flex justify-center items-center  ">
+        <img
+          className=" w-full border-b-2 border-t-2 rounded-3xl shadow-lg "
+          src=""
+        ></img>
+      </div>
+    </section>
+  );
+};
+export default Home;
