ddecdc9aa06c0db185915c43f9f76ded43489ba4
Djebril <djebrilacademy@gmail.com>
2024-05-01 12:49:32 +0100

    add contact + some style + finish hero

--- djebril-portfolio/src/pages/Contact.jsx
+++ djebril-portfolio/src/pages/Contact.jsx
@@ -1,4 +1,63 @@
-const Contact = ()=>{
+const Contact = () => {
+  return (
+    <section className="flex justify-center items-center w-full flex-col gap-16">
+      <div>
+        <h1>you can contact me by E-mail or phone number for pro contact </h1>
+        <h2>
+          and if you want to discuss with me in my field you can be comfortable
+          contacting me by my instagram account{" "}
+        </h2>
+      </div>
 
-}
-export default Contact
+      <div className="flex justify-center items-center gap-4 w-full">
+        <div className="border-2 border-black flex justify-center items-center gap-4 flex-col p-4 w-1/2">
+          <h1>my phone number</h1>
+          <h2>+213 556 94 65 14</h2>
+        </div>
+        <div className="border-2 border-black flex justify-center items-center gap-4 flex-col p-4 w-1/2">
+          <h1>my e-mail</h1>
+          <h2>Djebril.zidour.pro@gmail.com</h2>
+        </div>
+      </div>
+
+      <div className="flex justify-center items-center gap-16 ">
+        <div className="flex justify-center items-center flex-col gap-8">
+          <h1>check my github over here</h1>
+          <img
+            className="w-24"
+            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
+            alt=""
+          />
+        </div>
+
+        <div className="flex justify-center items-center flex-col gap-8">
+          <h1>check my instagram over here</h1>
+          <img
+            className="w-24"
+            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center flex-col gap-8">
+          <h1>check my facebook over here</h1>
+          <img
+            className="w-24"
+            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
+            alt=""
+          />
+        </div>
+        <div className="flex justify-center items-center flex-col gap-8">
+          <h1>check my likendin here</h1>
+          <img
+            className="w-24"
+            src="https://cdn-icons-png.flaticon.com/128/3792/3792702.png"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <h1>can't wait to work with you</h1>
+    </section>
+  );
+};
+export default Contact;
--- djebril-portfolio/src/pages/Home.jsx
+++ djebril-portfolio/src/pages/Home.jsx
@@ -1,29 +1,46 @@
+import { React } from "react";
+
+import {TypeAnimation} from "react-type-animation";
 const Home = () => {
   return (
-    <section className="flex justify-between items-center mx-12 my-8 bg-[url(https://i.ibb.co/RPYXJz6/Screen-Shot-2022-12-13-at-5-50-43-PM-copy.png)] bg-no-repeat bg-contain bg-center">
-      <div className="flex flex-col items-start justify-center w-1/2 gap-4">
-        <h1 className="text-5xl">Djebril Zidour </h1>
-        <p className="text-3xl">
-          {" "}
-          hi, im djebril im a web developer and this is my portfolio
-        </p>
-        <div className="flex justify-center items-center gap-4">
-          <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
-            click me{" "}
-          </button>
-          <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
-            {" "}
-            learn more
-          </button>
+    <>
+     
+
+      <section className="flex justify-between items-center  my-8 home px-12 ">
+        <div className="flex flex-col items-start justify-center w-1/2 gap-4">
+          <h1 className="text-5xl">My Name is <TypeAnimation
+        sequence={[
+          // Same substring at the start will only be typed out once, initially
+          "Djebril Zidour",
+          1000, // wait 1s before replacing "Mice" with "Hamsters"
+          "",
+          1000,
+          "Djebril Zidour",
+          1000
+         
+        ]}
+        wrapper="span"
+        speed={50}
+        style={{ fontSize: "1em", display: "inline-block" ,color: "#FC4100",}}
+        repeat={Infinity}
+      /></h1>
+          <p className="text-3xl">
+            an Algerian student living in algeirs , I'm 16 years old i have about 4 years in the field of web development 
+            actually  I have some hobbies that i enjoy doing it 
+            like photography <span className="text-red-600 cursor-pointer">check my pictures</span> and biking well you can see my skills on development by scrolling on this portfolio 
+          </p>
+          <div className="flex justify-center items-center gap-4">
+            <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
+              click me{" "}
+            </button>
+            <button className="border px-4 py-2 hover:bg-red-500 hover:text-white rounded-xl text-2xl capitalize uppercase">
+              {" "}
+              learn more
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="w-1/2 flex justify-center items-center  ">
-        <img
-          className=" w-full border-b-2 border-t-2 rounded-3xl shadow-lg "
-          src=""
-        ></img>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 export default Home;
